ef4014d4b68907a2cfa14c12f6e7422c718a2357
Dzmitry Shokel <dzmitry.shokel@draftboard.co>
2026-01-05 01:54:37 +0100

    fix asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     outDir: 'docs',
     emptyOutDir: true,
   },
-  base: '/guitar/',
-})
+  base: mode === 'production' ? '/guitar/' : '/',
+}))
